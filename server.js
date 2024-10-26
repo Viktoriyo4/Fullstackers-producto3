f185ad5420d9d9abd2acc63917caa68b0939c101
kevin seligmann <kseligmann@uoc.edu>
2024-10-26 17:47:28 +0200

    Agregado routing para controlador de panel y algunos comentarios

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+// App
 const express = require('express')
+
+// Config
 const config = require('./config/config')
-const database = require('./config/database')
-const dashboardController = require('./controllers/DashboardController')
+
+// Init bd
+const database= require('./config/database')
+
+
 const panel = require('./models/Panel')
 const task = require('./models/Task')
 
+// Controllers
+const dashboardController = require('./controllers/DashboardController')
+const panelController = require('./controllers/PanelController')
+
+// Start express
 const app = express()
 
+// Serve static
 app.use(express.static('public'))
 
+// Route controllers
 app.use('/dashboard', dashboardController)
+app.use('/panel', panelController)
 
+// Listen
 app.listen(config.port,  () => {console.log(`Listening on port: ${config.port}`)})
