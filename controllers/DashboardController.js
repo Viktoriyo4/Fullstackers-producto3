472f47b922050a3900ee1ab0092ec716210f4841
kevin seligmann <kseligmann@uoc.edu>
2024-10-26 17:27:54 +0200

    Agregado eschema de tasks

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -5,4 +5,5 @@ router.get('/', (req, res) => {
     res.send('hello world')
 })
 
+
 module.exports = router
--- models/Task.js
+++ models/Task.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-async function createTask(){
+async function createSchema(){
     const taskSchema = new mongoose.Schema({
         id: {type: Number, required: true},
-        name: {type: String, required: true},
+        title: {type: String, required: true},
+        description: {type: String, required: true},
+        dueDate: {type: Date, required: true},
+        assignee: {type: String, required: true},
+        idColumna: {type: Number, required:true}
     })
     
     const Task = mongoose.model('Task', taskSchema)
 }
-createTask()
+createSchema()
