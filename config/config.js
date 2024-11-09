75a2e27e5ef22336601820f445d79b10c6900bb2
victor <vrequenaga@uoc.edu>
2024-11-09 20:24:10 +0100

    conexion mongodb

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-const app_name = "fullstackers";
+const app_name = "fullstackmongo";
 const database_name = "fullstackers";
-const admin_username = "admin";
-const admin_password = "admin";
+const admin_username = "vrequenaga";
+const admin_password = "7Bg2CH5tDepNshab";
 const uri = `mongodb+srv://${admin_username}:${admin_password}@${app_name}.mhczk.mongodb.net/${database_name}?retryWrites=true&w=majority&appName=${app_name}`;
 const port = 8080;
 
