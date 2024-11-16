60c9fae75aa84b267c6930655991c723a521eefb
kevin seligmann <kseligmann@uoc.edu>
2024-11-16 17:30:26 +0100

    Config arreglada

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const app_name = "fullstackmongo";
 const database_name = "fullstackers";
-const admin_username = "vrequenaga";
-const admin_password = "7Bg2CH5tDepNshab";
+const admin_username = "admin";
+const admin_password = "admin";
 const uri = `mongodb+srv://${admin_username}:${admin_password}@${app_name}.mhczk.mongodb.net/${database_name}?retryWrites=true&w=majority&appName=${app_name}`;
 const port = 8080;
 
