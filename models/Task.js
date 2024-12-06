74b592c9ae1da769dd4296932bb98f8b88b97d0d
Albert Recuero Campasol <albertrc7573@gmail.com>
2024-12-06 20:44:42 +0100

    Boton para agregar Files

--- models/Task.js
+++ models/Task.js
@@ -5,7 +5,8 @@ const taskSchema = new mongoose.Schema({
     description: {type: String, required: true},
     dueDate: {type: Date, required: true},
     assignee: {type: String, required: true},
-    columnId: {type: Number, required:true}
+    columnId: {type: Number, required:true},
+    file: [{data: Buffer, contentType: String}]
 })
 
 const Task = mongoose.model('Task', taskSchema)
